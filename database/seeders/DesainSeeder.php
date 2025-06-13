db4e5b1c79b1d0d1c769dc866660487a68cf972e
rafi.hidayat1414 <rafi.hidayat1414@gmail.com>
2025-06-13 15:14:54 +0000

    Update seeder

--- database/seeders/DesainSeeder.php
+++ database/seeders/DesainSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Desain;
 
 class DesainSeeder extends Seeder
 {
@@ -12,6 +13,18 @@ class DesainSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Desain::create([
+            'judul' => 'Desain Minimalis 1 Lantai',
+            'luas' => '72 m²',
+            'harga' => 150000000,
+            'imageUrl' => 'upload/minimalis1.jpg',
+        ]);
+
+        Desain::create([
+            'judul' => 'Desain Modern 2 Lantai',
+            'luas' => '120 m²',
+            'harga' => 300000000,
+            'imageUrl' => 'upload/modern2lantai.jpg',
+        ]);
     }
 }
